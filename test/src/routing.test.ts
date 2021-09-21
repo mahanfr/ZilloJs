d822f5ad4e8b177e31f27b3fe2f0e587a673743f
mahanfarzaneh2000 <mahan.farzaneh2@gmail.com>
2021-09-21 18:01:24 +0430

    First test for routing

---
+++ test/src/routing.test.ts
@@ -0,0 +1,9 @@
+import {isRouteMatchsUrl} from "../../src/routing"
+
+describe('Routing', () => {
+    it('should return if route is equal to url', () =>{
+        expect(isRouteMatchsUrl('/products/5/?id=2112','/products/$id/')).toBeTruthy();
+        expect(isRouteMatchsUrl('/products/5/','/carts/$id/')).toBeFalsy();
+        expect(isRouteMatchsUrl('/products/abc/','/products/abc/')).toBeTruthy();
+    })
+})
