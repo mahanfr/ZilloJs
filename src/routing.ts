1dbbfbd5ae5d77c9792c10f19e02e4268e2aef21
mahanfarzaneh2000 <mahan.farzaneh2@gmail.com>
2021-09-25 14:48:28 +0330

    Application and Routing created

--- src/routing.ts
+++ src/routing.ts
@@ -1,40 +1,105 @@
 
-const routeList : string[] = []
+enum Method{
+  GET,POST,UPDATE,DELETE
+}
 
-function parseToRegex(url:string):string {
-  let str:string = "";
-
-  for (var i =0; i < url.length; i++) {
-    const c = url.charAt(i);
-    if (c === "$") {
-      // eat all characters
-      let param:string = "";
-      for (var j = i + 1; j < url.length; j++) {
-        if (/\w/.test(url.charAt(j))) {
-          param += url.charAt(j);
-        } else {
-          break;
-        }
-      }
-      str += `(?<${param}>\\w+)`;
-      i = j - 1;
-    } else {
-      str += c;
-    }
+class Route {
+  method: Method
+  helper: string
+  url:string
+  
+  constructor(url:string, method:Method, helper:string="text/html") {
+    this.url = url;
+    this.method = method;
+    this.helper = helper;
   }
-  return str;
 }
 
-function getPramRouteValues(url:string,route:string): {[key:string]:string; } | undefined {
-  const regex = parseToRegex(route);
-  const match = url.match(new RegExp(regex));
-  return match?.groups
+var Routes = (function(){
+  function Routes() {
+    //do stuff
+  }
+  var instance: any;
+  return {
+    getInstance: function(){
+        if (instance == null) {
+          instance = new _Routes();
+          // Hide the constructor so the returned object can't be new'd...
+          instance.constructor = null;
+        }
+        return instance;
+    }
+  };
+})();
+
+class _Routes{
+
+  routes: {[key:string]:Route}
+
+  constructor(){
+    this.routes = {}
+  }
+
+  public addRoute(url:string, method:Method, helper?:string){
+    let route = new Route(url, method, helper)
+    this.routes[url] = route
+  }
+
+  public parseQuery(url:string) {
+    const results = url.match(/\?(?<query>.*)/);
+    if (!results) {
+      return {};
+    }
+    const { groups: { query } } : any = results;
+  
+    const pairs = query.match(/(?<param>\w+)=(?<value>\w+)/g);
+    if(pairs === null){
+      return {};
+    }
+    const params = pairs.reduce((acc:any, curr:any) => {
+      const [key, value] = curr.split(("="));
+      acc[key] = value;
+      return acc;
+    }, {});
+    return params;
+  }
+  
+  public parseToRegex(url:string):string {
+    let str:string = "";
+  
+    for (var i =0; i < url.length; i++) {
+      const c = url.charAt(i);
+      if (c === "$") {
+        // eat all characters
+        let param:string = "";
+        for (var j = i + 1; j < url.length; j++) {
+          if (/\w/.test(url.charAt(j))) {
+            param += url.charAt(j);
+          } else {
+            break;
+          }
+        }
+        str += `(?<${param}>\\w+)`;
+        i = j - 1;
+      } else {
+        str += c;
+      }
+    }
+    return str;
+  }
+  
+  public getPramRouteValues(url:string,route:string): {[key:string]:string; } | undefined {
+    const regex = this.parseToRegex(route);
+    const match = url.match(new RegExp(regex));
+    return match?.groups
+  }
+  
+  public isRouteMatchsUrl(url:string, route:string): boolean{
+    url = url.split('?')[0]
+    const regex = new RegExp(this.parseToRegex(route));
+    return regex.test(url)
+  }
+
 }
 
-function isRouteMatchsUrl(url:string, route:string): boolean{
-  url = url.split('?')[0]
-  const regex = new RegExp(parseToRegex(route));
-  return regex.test(url)
-}
-
-export {parseToRegex, isRouteMatchsUrl, getPramRouteValues};
+export { Routes, Route, Method};
