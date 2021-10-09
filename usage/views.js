d34c87fa03da5d33593e50d3fb69794dd8f0e763
mahan <mahan.farzaneh@gmail.com>
2021-10-09 20:41:11 +0330

    context added to views and template updated

--- usage/views.js
+++ usage/views.js
@@ -1,8 +1,16 @@
 import { render } from '../lib/views.js'
 
+const indexContext = {
+    "title":"zillojs",
+    "user":{
+        "name":"ali",
+        "age":20
+    }
+}
+
 
 function indexView(request){
-    return render(request, 'usage/template/index.html')
+    return render(request, 'usage/template/index.html',indexContext)
     // return response(request, json, code)
 }
 
