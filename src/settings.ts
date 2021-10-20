d3aba441c9c17bd77340fd7a1149391c1ea4aba1
mahanfarzaneh2000 <mahan.farzaneh2@gmail.com>
2021-10-20 22:28:40 +0330

    settings file added

---
+++ src/settings.ts
@@ -0,0 +1,13 @@
+interface setting{
+    appName:string;
+    rootFile:string;
+    debug:Boolean;
+    secretKey:string;
+    Database:string;
+    DatabaseName:string;
+    DatabasePassword:string;
+}
+
+function getDataFromProjectJson(){
+    throw new Error("Not implemented yet!");
+}
